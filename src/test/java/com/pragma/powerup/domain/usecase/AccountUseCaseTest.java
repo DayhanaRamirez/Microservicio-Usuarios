a18a0466280fcead21b84c232d45b5c8095634cc
Dayhana Ramirez <dayhana.ramirez@pragma.com.co>
2023-07-06 00:09:17 -0500

    save before security

--- src/test/java/com/pragma/powerup/domain/usecase/AccountUseCaseTest.java
+++ src/test/java/com/pragma/powerup/domain/usecase/AccountUseCaseTest.java
@@ -1,4 +1,33 @@
+package com.pragma.powerup.domain.usecase;
+
+import com.pragma.powerup.domain.spi.IAccountPersistencePort;
+import com.pragma.powerup.infrastructure.output.jpa.adapter.AccountJpaAdapter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AccountUseCaseTest {
-  
+
+    private AccountJpaAdapter accountJpaAdapter;
+
+    @Test
+    void saveAccount() {
+    }
+
+    @Test
+    void getAllAccounts() {
+    }
+
+    @Test
+    void getAccount() {
+    }
+
+    @Test
+    void updateAccount() {
+    }
+
+    @Test
+    void deleteAccount() {
+    }
 }
