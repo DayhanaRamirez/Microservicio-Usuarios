8950eca637a270ff9f855131ace020120ff4ef96
Dayhana Ramirez <dayhana.ramirez@pragma.com.co>
2023-07-09 09:12:50 -0500

    Commit

---
+++ src/main/java/com/pragma/powerup/infrastructure/configuration/security/ApplicationConfiguration.java
@@ -0,0 +1,2 @@
+package com.pragma.powerup.infrastructure.configuration.security;public class ApplicationConfiguration {
+}
---
+++ src/main/java/com/pragma/powerup/infrastructure/configuration/security/WebSecurityConfiguration.java
@@ -0,0 +1,2 @@
+package com.pragma.powerup.infrastructure.security;public class WebSecurityConfiguration {
+}
